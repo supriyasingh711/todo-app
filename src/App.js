72691d99bf4cace1076415eeed71220bb928b87b
supriya singh <108217678+supriyasingh711@users.noreply.github.com>
2023-11-17 15:49:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component }  from 'react';
+import Home from './pages/Home/Home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+<div className='board-row'>
+  <button className='square'>1</button>
+  <button className='square'>2</button>
+  <button className='square'>3</button>
+
+
+</div>
+  </>
+     
+   
+   
   );
 }
 
